d1724ad5f99d3a93fc828b8ae32d2a3a1ac82711
Alejandro Tabares Bosquet <alex311es@gmail.com>
2019-12-13 17:44:40 +0000

    Cleanup

--- src/state/actions/active-video-ready.ts
+++
@@ -1,6 +0,0 @@
-import { ActionTypes } from './action-types'
-
-export const setActiveVideoReady = (ready: boolean) => ({
-  type: ActionTypes.SET_ACTIVE_VIDEO_READY,
-  ready
-})
--- src/state/actions/active-video.ts
+++
@@ -1,6 +0,0 @@
-import { ActionTypes } from './action-types'
-
-export const setActiveVideo = (id: number) => ({
-  type: ActionTypes.SET_ACTIVE_VIDEO,
-  id
-})
